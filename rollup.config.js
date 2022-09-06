140d8530b978f4d3b42b6f695ec859ee4b2abd7a
unknown <3478437011@qq.com>
2022-09-06 11:56:37 +0800

    测试

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import vue from 'rollup-plugin-vue'
+export default{
+    input :'./src/App.vue',
+    output:{
+        // es module
+        format:'es',
+        name:'vue',//import XX from 'vue'->vue.esm.js        //文件名称
+        file:"vue.esm.js",
+
+    },
+    plugins:[vue()]
+}
